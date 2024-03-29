0320d4ef9fe27fdcc32e0709ce824d586aa18367
MartinKalema <kalemaaaaaaaa@gmail.com>
2024-03-29 18:36:41 +0300

    notes

--- template.py
+++ template.py
@@ -27,6 +27,7 @@ list_of_files = [
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/entity/entity_configuration.py",
     f"src/{project_name}/constants/__init__.py",
+    f"src/notes.txt",
     "config/config.yaml",
     "dvc.yaml",
     "params.yaml",
